b3e1bf8f1680f0cc5e46c0c4a6c74382d49ae8c5
lyn1027 <cr041027@gmail.com>
2026-04-08 09:13:49 +0900

    Create Header.js

---
+++ 0401/Header.js
@@ -0,0 +1,9 @@
+// 상단 헤더 컴포넌트
+const Header = () => {
+  return (
+    <header style={{ padding: '20px', backgroundColor: '#f0f0f0' }}>
+      <h1>Google Antigravity Clone</h1>
+    </header>
+  );
+};
+export default Header;
