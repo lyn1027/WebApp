5c515fd96c2401063da76d7c6fdb4ac297d201ee
lyn1027 <cr041027@gmail.com>
2026-04-08 09:18:41 +0900

    Delete 0401/Footer.js

--- 0401/Footer.js
+++
@@ -1,9 +0,0 @@
-// 하단 푸터 컴포넌트
-const Footer = () => {
-  return (
-    <footer style={{ padding: '20px', textAlign: 'center', fontSize: '0.8rem' }}>
-      <p>&copy; 2026 Chae-rin Kim. WebApp Assignment.</p>
-    </footer>
-  );
-};
-export default Footer;
