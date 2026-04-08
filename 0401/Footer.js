9fd73de2c35391a01b8e96dc1b484a8e4b0cd23f
lyn1027 <cr041027@gmail.com>
2026-04-08 09:14:16 +0900

    Create Footer.js

---
+++ 0401/Footer.js
@@ -0,0 +1,9 @@
+// 하단 푸터 컴포넌트
+const Footer = () => {
+  return (
+    <footer style={{ padding: '20px', textAlign: 'center', fontSize: '0.8rem' }}>
+      <p>&copy; 2026 Chae-rin Kim. WebApp Assignment.</p>
+    </footer>
+  );
+};
+export default Footer;
