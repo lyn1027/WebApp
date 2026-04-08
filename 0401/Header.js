a19cfcac0f93d75457160f50a1adfc244951e0dd
lyn1027 <cr041027@gmail.com>
2026-04-08 09:18:48 +0900

    Delete 0401/Header.js

--- 0401/Header.js
+++
@@ -1,9 +0,0 @@
-// 상단 헤더 컴포넌트
-const Header = () => {
-  return (
-    <header style={{ padding: '20px', backgroundColor: '#f0f0f0' }}>
-      <h1>Google Antigravity Clone</h1>
-    </header>
-  );
-};
-export default Header;
